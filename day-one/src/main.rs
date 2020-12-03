559650152ca22c29f5429d0d781fc4ab47d72c73
Joseph Kerkhof <musicaljoeker@gmail.com>
2020-12-02 22:18:14 -0600

    formatting fixes

--- day-one/src/main.rs
+++ day-one/src/main.rs
@@ -6,27 +6,27 @@ use std::process;
 fn problem_one() {
     // reading in the file -- O(n)
     let input = fs::read_to_string("input.txt")
-    .expect("Something went wrong reading the input file");
+        .expect("Something went wrong reading the input file");
 
     // converting file to an array of i32s -- O(n)
     let entries_int: Vec<i32> = input
-    .split_whitespace()
-    .map(|s| s.parse().expect("parse error"))
-    .collect();
+        .split_whitespace()
+        .map(|s| s.parse().expect("parse error"))
+        .collect();
 
     // converting i32 array into a hashset for quick lookup -- O(n)
     let entries_hs: HashSet<i32> = entries_int
-    .clone()
-    .into_iter()
-    .collect();
+        .clone()
+        .into_iter()
+        .collect();
 
     // find the complement and compute the answer -- O(n)
     for e in entries_int {
-    let complement = 2020 - e;
-    if entries_hs.contains(&complement) {
-        println!("Answer: {}", e * complement);
-        process::exit(0); // done!
-    }
+        let complement = 2020 - e;
+        if entries_hs.contains(&complement) {
+            println!("Answer: {}", e * complement);
+            process::exit(0); // done!
+        }
     }
 }
 
